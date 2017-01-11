98dc8c9e5456d2df3ad70d4be5749618fbab204b
njaramillo-ng <njaramillo@nemorisgames.com>
2017-01-11 16:30:55 -0300

    direccion en dataBase

--- Assets/ModuloGenerico/Scripts/DataBase.cs
+++ Assets/ModuloGenerico/Scripts/DataBase.cs
@@ -8,6 +8,7 @@ using MySql.Data.MySqlClient;
 /// </summary>
 public class DataBase
 {
+	public string direccion = "http://localhost/";
     private string source;
     private MySqlConnection conexion;
     public bool conectado = false;
