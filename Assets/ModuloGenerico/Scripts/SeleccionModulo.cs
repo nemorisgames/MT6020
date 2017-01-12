7d7773bd981d12a91b6f33fdee396da78e1090ea
sleivanemoris <sleivanemoris@nemorisgames.com>
2017-01-12 14:41:03 -0300

    seleccion modulo a php

--- Assets/ModuloGenerico/Scripts/SeleccionModulo.cs
+++ Assets/ModuloGenerico/Scripts/SeleccionModulo.cs
@@ -14,9 +14,10 @@ public class SeleccionModulo : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
-        datos = db.Consultar("SELECT * FROM module");
+        //datos = db.Consultar("SELECT * FROM module");
         
-        StartCoroutine(espera(datos));
+        //StartCoroutine(espera(datos));
+        StartCoroutine(wait());
     }
 
     public void crearModulo()
@@ -33,6 +34,7 @@ public class SeleccionModulo : MonoBehaviour {
 
     IEnumerator espera(MySqlDataReader datos)
     {
+
         for (int i = 0; i < 500; i++)
         {
             yield return new WaitForSeconds(0.1f);
@@ -54,5 +56,22 @@ public class SeleccionModulo : MonoBehaviour {
             }
         }
         Debug.Log("Información no recibida");
+    }
+
+    IEnumerator wait(){
+        WWW download = new WWW(db.direccion + "seleccionModulo.php");
+        yield return download;
+
+        string retorno = download.text;
+        print(retorno);
+        string[] ret = retorno.Split(new char[]{'|'});
+        for(int i=0;i<ret.Length-1;i++){
+            string[] ret2=ret[i].Split(new char[]{'*'});
+            print ("asd: " + ret2[0]);
+            print ("asdds: " + ret2[1]);
+            nombre = (string)ret2[1];
+            print("nombre: " + nombre);
+            CBSeleccionTipo.AddItem(nombre);
+        }
     }
 }
