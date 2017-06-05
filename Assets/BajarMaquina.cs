559deafb02c6a4469d0238f017ec6b12c2c3ae57
njaramillo-ng <njaramillo@nemorisgames.com>
2017-06-05 11:19:49 -0400

    fixes modulos 4 y 7

--- Assets/BajarMaquina.cs
+++ Assets/BajarMaquina.cs
@@ -6,11 +6,12 @@ public class BajarMaquina : MonoBehaviour {
 
 	public bool triggerEnabled = false;
 
-	void OnTriggerExit(Collider c){
+	void OnTriggerEnter(Collider c){
 		if (c.name == "ControlUsuarioChecklist" && triggerEnabled) {
 			Vector3 pos = c.transform.FindChild("Camera").transform.position;
 			pos.y += 0.7f;
 			c.transform.FindChild ("Camera").transform.position = pos;
+			triggerEnabled = false;
 		}
 	}
 }
