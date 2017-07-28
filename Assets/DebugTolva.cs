e445e49c50538523574ccd202aad0ba4042de836
njaramillo-ng <njaramillo@nemorisgames.com>
2017-07-28 15:03:40 -0400

    revision tolva

---
+++ Assets/DebugTolva.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTolva : MonoBehaviour {
+	UILabel label;
+
+	// Use this for initialization
+	void Start () {
+		label = GetComponent<UILabel> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		#if UNITY_EDITOR
+		label.text = "tolva: " + Input.GetAxis("ControlTolbaEditor");
+		#else
+		label.text = "tolva: " + Input.GetAxis ("ControlTolba");	
+		#endif
+	}
+}
