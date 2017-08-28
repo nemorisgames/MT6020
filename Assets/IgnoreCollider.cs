1bd078e95b15ff2aba2fd63679ba953bd2e1bb21
njaramillo-ng <njaramillo@nemorisgames.com>
2017-08-28 15:48:49 -0300

    final

---
+++ Assets/IgnoreCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgnoreCollider : MonoBehaviour {
+	public string layer = "IgnoreCollision";
+	Collider c;
+
+	// Use this for initialization
+	void Start () {
+		c = GetComponent<Collider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision collision){
+		if (collision.gameObject.layer == LayerMask.NameToLayer (layer)) {
+			Physics.IgnoreCollision (collision.collider, c);
+		} else
+			Debug.Log (collision.gameObject.name);
+	}
+}
